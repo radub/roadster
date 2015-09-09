ed3fd40156f6a81c85a9fc235ad9ba47c295a49d
Radu Barbu <radu.barbu@gmail.com>
2015-09-09 14:49:16 +0300

    Enable php5-mods; Configure xdebug

--- environments/local/modules/php/manifests/init.pp
+++ environments/local/modules/php/manifests/init.pp
@@ -17,11 +17,33 @@ class php {
     "libapache2-mod-php5"
   ]
 
-  # @TODO enable php5-mcrypt: sudo php5enmod mcrypt
-
   package { $packages:
     ensure => present,
     require => Exec["apt-get update"]
+  }
+
+  exec { 'enable-php5-mcrypt':
+    command => 'php5enmod mcrypt',
+    require => [ Package['php5-mcrypt'], Package['apache2'] ],
+    notify  => Service["apache2"]
+  }
+
+  exec { 'enable-php5-gd':
+    command => 'php5enmod gd',
+    require => [ Package['php5-gd'], Package['apache2'] ],
+    notify  => Service["apache2"]
+  }
+
+  exec { 'enable-php5-ldap':
+    command => 'php5enmod ldap',
+    require => [ Package['php5-ldap'], Package['apache2'] ],
+    notify  => Service["apache2"]
+  }
+  
+  file { "/etc/php5/mods-available/xdebug.ini":
+    ensure => present,
+    source => "/vagrant/environments/local/modules/php/assets/xdebug.conf",
+    require => [ Package["apache2"], Package["php5-xdebug"] ]
   }
 
 }
