881e765a3bde01fe5f63da7068b5c24128755a99
radone <radu.barbu@gmail.com>
2015-11-12 17:06:20 +0200

    Fix gulp install

--- environments/local/modules/tools/manifests/nodejs.pp
+++ environments/local/modules/tools/manifests/nodejs.pp
@@ -6,12 +6,28 @@ class tools::nodejs {
   ]
 
   package { $packages:
-    ensure => present,
+    ensure  => present,
     require => Exec["apt-get update"]
+  }
+
+  # fix node nodejs legacy issue
+  file { '/usr/bin/node':
+    ensure => 'link',
+    target => '/usr/bin/nodejs',
+  }
+
+  exec { 'gem-install-sass':
+    command => 'gem install sass',
+    path    => '/usr/bin',
+    user    => root
   }
 
   exec { 'install-gulp':
     command => 'npm install gulp -g',
+    require => Package['npm']
+  } ->
+  exec { 'install-gulp-save-dev':
+    command => 'npm install gulp --save-dev',
     require => Package['npm']
   }
 
