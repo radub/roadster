efc28738286a63e14f7410c4b2985cdd910c5bd9
Radu Barbu <radu.barbu@gmail.com>
2016-04-01 14:54:04 +0300

    Update init.pp

--- environments/local/modules/tools/manifests/init.pp
+++ environments/local/modules/tools/manifests/init.pp
@@ -15,19 +15,20 @@ class tools {
     require => Exec["apt-get update"]
   }
 
-  exec { 'composer-install':
-    command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
-    path    => '/usr/bin:/usr/sbin',
-    require => Package['curl']
-  } ->
-  file { '/etc/profile.d/add-composer-to-path.sh':
-    ensure  => present,
-    content => 'export PATH="$HOME/.composer/vendor/bin:$PATH"'
-  }
+#  exec { 'composer-install':
+#    command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
+#    path    => '/usr/bin:/usr/sbin',
+#    require => Package['curl']
+#  } ->
+#  file { '/etc/profile.d/add-composer-to-path.sh':
+#    ensure  => present,
+#    content => 'export PATH="$HOME/.composer/vendor/bin:$PATH"'
+#  }
 
-  exec { 'drush-install':
-    command     => '/usr/local/bin/composer global require drush/drush:dev-master',
-    environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
-    require     => Exec['composer-install']
-  }
+#  exec { 'drush-install':
+#    command     => '/usr/local/bin/composer global require drush/drush:dev-master',
+#    environment => ["COMPOSER_HOME=/home/vagrant/.composer"],
+#    require     => Exec['composer-install']
+#  }
+
 }
